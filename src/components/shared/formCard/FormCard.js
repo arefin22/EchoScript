0e1c298edb64a95f742a5f9d99bc2648943d6d89
Afiya32 <biplobguru123@gmail.com>
2024-01-18 10:06:48 +0600

    form for home page completed with updated without material ui

--- src/components/shared/formCard/FormCard.js
+++ src/components/shared/formCard/FormCard.js
@@ -1,142 +1,108 @@
 "use client"
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import { styled } from '@mui/material/styles';
-import  CloudUploadIcon  from '@mui/icons-material/CloudUpload';
-// import SendIcon from '@mui/icons-material'
-import TextField from '@mui/material/TextField';
-import { Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, InputLabel, MenuItem, Radio, RadioGroup, Select } from '@mui/material';
-import { SendAndArchiveOutlined } from '@mui/icons-material';
 
 const FormCard = () => {
-    const [option, setOption] = React.useState('');
-    const handleSubmit=()=>{
-        console.log("submitted");
-    }
-    const handleOption = (event) => {
-        setOption(event.target.value);}
-        const VisuallyHiddenInput = styled('input')({
-            clip: 'rect(0 0 0 0)',
-            clipPath: 'inset(50%)',
-            height: 1,
-            overflow: 'hidden',
-            position: 'absolute',
-            bottom: 0,
-            left: 0,
-            whiteSpace: 'nowrap',
-            width: 1,
-          });
+   
   return (
     <div className='mx-auto'>
-      <form onSubmit={handleSubmit}>
-       <div className='m-2 p-2' >
-       <Box 
-          sx={{
-            border:'none',
-            width: 500,
-            maxWidth: '100%',
-            backgroundColor: '#D9D9D9',
-            border:'none'
-       
-          }}
-        >
-          <TextField className='text-black border-none' fullWidth label="Input Something Here" id="fullWidth" />
-        </Box>
-       </div>
-       <div className='m-2 p-2' >
-       <Box 
-          sx={{
-            border:'none',
-            width: 500,
-            maxWidth: '100%',
-            backgroundColor: '#D9D9D9',
-            border:'none'
-       
-          }}
-        >
-          <TextField className='text-black border-none' fullWidth label="Input Something Here" id="fullWidth" />
-        </Box>
-       </div>
-       <div className='m-2 p-2' >
-       <Box 
-          sx={{
-            border:'none',
-            width: 500,
-            maxWidth: '100%',
-            backgroundColor: '#D9D9D9',
-            border:'none'
-       
-          }}
-        >
-          <TextField className='text-black border-none' fullWidth label="Input Something Here" id="fullWidth" />
-        </Box >
-       </div>
-       <div className='m-2 p-2' >
-       <Box 
-          sx={{
-            border:'none',
-            width: 500,
-            maxWidth: '100%',
-            backgroundColor: '#D9D9D9',
-            border:'none'
-       
-          }}
-        >
-          <TextField className='text-black border-none' fullWidth label="Input Something Here" id="fullWidth" />
-        </Box >
-       </div>
-       <div className='m-2 p-2'>
-       <FormGroup>
-  <FormControlLabel control={<Checkbox defaultChecked />} label="Terms and Conditions" />
-  <FormControlLabel required control={<Checkbox />} label="Security Policy" />
-
-</FormGroup>
-       </div>
-       <div className='m-2 p-2'>
-       <FormControl>
-      <FormLabel id="demo-row-radio-buttons-group-label">Gender</FormLabel>
-      <RadioGroup
-        row
-        aria-labelledby="demo-row-radio-buttons-group-label"
-        name="row-radio-buttons-group"
-      >
-        <FormControlLabel value="female" control={<Radio />} label="Female" />
-        <FormControlLabel value="male" control={<Radio />} label="Male" />
-        <FormControlLabel value="other" control={<Radio />} label="Other" />
-      
-      </RadioGroup>
-    </FormControl>
-       </div>
-       <div className='m-2 p-2'>
-       <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-      <InputLabel id="demo-select-small-label">Select Option</InputLabel>
-      <Select
-        labelId="demo-select-small-label"
-        id="demo-select-small"
-        value={option}
-        label="Option"
-        onChange={handleOption}
-      >
-       
-        <MenuItem value="option-1">Option-1</MenuItem>
-        <MenuItem value="option-2">Option-2</MenuItem>
-        <MenuItem value="option-3">Option-3</MenuItem>
-      </Select>
-    </FormControl>
-       </div>
-       <div className='m-2 p-2'>
-       <Button component="label" variant="contained" startIcon={<CloudUploadIcon />}>
-      Upload file
-      <VisuallyHiddenInput type="file" />
-    </Button>
-       </div>
-       <div className='m-2 p-2'>
-       <Button type="submit" variant="contained" endIcon={<SendAndArchiveOutlined />}>
-  Submit
-</Button>
-       </div>
      
-      </form>
+      <div className='w-3/4 mx-auto'>
+         <form  className="mt-8 mb-2 w-full ">
+    <div className="mb-4 flex flex-col gap-6">
+      <div className="relative h-11 w-full min-w-[200px]">
+        <input
+          className="peer bg-[#D9D9D9] h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+        type="name" name="name" 
+        />
+        <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+          Input Something here
+        </label>
+      </div>
+      <div className="relative h-11 w-full min-w-[200px]">
+        <input
+          className="peer h-full w-full bg-[#D9D9D9] rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+        type="name" name="photoURL" 
+        />
+        <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+        Input Something here
+        </label>
+      </div>
+      <div className="relative h-11 w-full min-w-[200px]">
+        <input
+          className="peer h-full bg-[#D9D9D9] w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+           type="text" name="email" 
+        />
+        <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+        Input Something here
+        </label>
+      </div>
+      <div className="relative h-11 w-full min-w-[200px]">
+        <input
+          type="text" name="password"
+          className="peer h-full bg-[#D9D9D9] w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+        
+        />
+        <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+        Input Something here
+        </label>
+      </div>
+    </div>
+    <div className="inline-flex items-center">
+      
+      
+    </div>
+    <div className='m-2 p-2'>
+    <div className="form-control">
+  <label className="label cursor-pointer">
+    
+    <input type="checkbox"  className="checkbox bg-[#D9D9D9]" />
+    <span className="label-text">Terms and Conditions</span> 
+  </label>
+</div>
+    </div>
+    <div className='m-2 p-2'>
+    <div className="form-control">
+  <label className="label cursor-pointer">
+    
+    <input type="checkbox"  className="checkbox bg-[#D9D9D9]" />
+    <span className="label-text">Security Policy</span> 
+  </label>
+</div>
+    </div>
+   
+    <div>
+      <div className='m-2 p-2 bg-[#D9D9D9] rounded-2xl'>
+      <select className="select bg-[#D9D9D9] w-full rounded-2xl select-accent ">
+  <option disabled selected>Gender</option>
+  <option>Male</option>
+  <option>Female</option>
+  <option>Other</option>
+</select>
+      </div>
+    </div>
+    <div>
+      <div className='m-2 p-2 bg-[#D9D9D9] rounded-2xl'>
+      <select className="select bg-[#D9D9D9] w-full rounded-2xl select-accent  ">
+  <option disabled selected>Option</option>
+  <option>Option-1</option>
+  <option>Option-2</option>
+  <option>Option-3</option>
+</select>
+      </div>
+      <div>
+      <input type="file" className="file-input file-input-bordered w-full bg-[#D9D9D9] rounded-2xl" />
+      </div>
+    </div>
+    <button
+      className="mt-6 block w-full select-none rounded-lg bg-[#D9D9D9] py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-black shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      type='submit'
+      data-ripple-light="true"
+    >
+     Submit
+    </button>
+    
+  </form>
+      </div>
     </div>
   );
 };
