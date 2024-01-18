e06c5b45a7fdd5311b855e9745e906daaa5ac168
Shawal <shahishawal@gmail.com>
2024-01-18 13:05:12 +0600

    section:Home[Banner Section

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
       
 import Article from "@/components/Article/Article";
+import Banner from "@/components/Banner/Banner";
 import ButtonWithBgColor from "@/components/Button/ButtonWithBgColor";
 import ButtonWithoutBgColor from "@/components/Button/ButtonWithoutBgColor";
 import Card from "@/components/Card/Card";
 
 const page = () => {
   return (
-    <div>    
-        <h1>Welcome to EchoScript</h1>
+    <div className="">  
+        <Banner></Banner>  
+        
     </div>
   );
 };
