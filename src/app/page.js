ca2a50fc85155b6d2695b77ff90853c670b6da14
Shamsul Arefin <shamsularefin500@gmail.com>
2024-01-18 21:20:42 +0600

    [Content](About Us):Added Some Text in about Us Section

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,8 @@
-
-import Article from "@/components/Article/Article";
 import Banner from "@/components/Banner/Banner";
-import ButtonWithBgColor from "@/components/Button/ButtonWithBgColor";
-import ButtonWithoutBgColor from "@/components/Button/ButtonWithoutBgColor";
-import Card from "@/components/Card/Card";
 
 const page = () => {
   return (
-    <div className="">  
+    <div>  
         <Banner></Banner>  
         
     </div>
