7b7b9b7e74d3c93cbdd597c73d471939bb4751b2
Shamsul Arefin <shamsularefin500@gmail.com>
2024-01-22 10:54:39 +0600

    Added two Folder as BackEnd And Front End

--- src/components/Button/ButtonWithoutBgColor.js
+++
@@ -1,9 +0,0 @@
-const ButtonWithoutBgColor = ({name}) => {
-  return (
-    <div>
-      <button className="px-12 py-3 rounded-3xl border-2 hover:bg-[#025] border-[#025] text-[#025] hover:text-white">{name}</button>
-    </div>
-  );
-};
-
-export default ButtonWithoutBgColor;
