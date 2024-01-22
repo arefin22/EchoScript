c7c448e4db5a7dd5ec8f07f4e4dba1f7c201581d
Shamsul Arefin <shamsularefin500@gmail.com>
2024-01-22 10:54:39 +0600

    Added two Folder as BackEnd And Front End

--- src/components/Button/ButtonWithBgColor.js
+++
@@ -1,9 +0,0 @@
-const ButtonWithBgColor = ({name}) => {
-  return (
-    <div>
-      <button className="bg-[#025] text-white px-12 py-3 rounded-3xl">{name}</button>
-    </div>
-  );
-};
-
-export default ButtonWithBgColor;
