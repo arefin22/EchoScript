b479ac78ddf2362ed6169ae4eef47beaf9567ae1
tanimtorikul <torikulswe@gmail.com>
2024-01-23 19:50:44 +0600

    article card implemented in home page

--- FrontEnd/src/app/page.js
+++ FrontEnd/src/app/page.js
@@ -1,12 +1,36 @@
 import Article from "@/components/Article/Article";
 import Banner from "@/components/Banner/Banner";
 import ButtonWithBgColor from "@/components/Button/ButtonWithBgColor";
+import Card from "@/components/Card/Card";
 import TextEditor from "@/components/TextEditor/TextEditor";
+import cardData from "../utils/cardData";
 
 const page = () => {
   return (
     <div>
       <Banner></Banner>
+      {/* popular cards */}
+      <div className="my-24">
+      <h1 className="text-start text-[#4C2F17] text-3xl font-light mb-5">
+        Start Reading Now...
+      </h1>
+      <div className="my-32 grid grid-cols-1 md:grid-cols-3 gap-10">
+        {cardData.map((card, index) => (
+          <Card
+            key={index}
+            tags={card.tags}
+            title={card.title}
+            date={card.date}
+            likes={card.likes}
+            comments={card.comments}
+            article={card.article}
+            image={card.image}
+            authorName={card.authorName}
+            view={card.view}
+          />
+        ))}
+      </div>
+    </div>
       <div className="my-24">
         <h1 className="text-center text-3xl font-extrabold mb-5">
           Latest Article
@@ -107,11 +131,10 @@ const page = () => {
         </div>
       </div>
       <div>
-        <TextEditor/>
+        <TextEditor />
       </div>
     </div>
   );
 };
 
 export default page;
-
