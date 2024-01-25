80d4e9caf2220d9643c669164b08f082173b3371
Afiya32 <biplobguru123@gmail.com>
2024-01-25 21:52:01 +0600

    authentacation completed without no bug

--- FrontEnd/src/app/layout.js
+++ FrontEnd/src/app/layout.js
@@ -2,6 +2,8 @@ import Footer from '@/components/shared/Footer'
 import Navbar from '@/components/shared/Navbar'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { AuthProvider } from '@/context/authContext';
+import { Toaster } from 'react-hot-toast';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,11 +16,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+      <AuthProvider>
+      <div><Toaster/></div>
         <div className="max-w-screen-full mx-auto">
           <Navbar />
           <div className='container mx-auto'>{children}</div>
           <Footer />
         </div>
+        </AuthProvider>
       </body>
     </html>
   );
