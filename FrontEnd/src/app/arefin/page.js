34b9b8578e84da266a93832941b0f2fbaf6c5f15
Shamsul Arefin <shamsularefin500@gmail.com>
2024-01-26 08:43:20 +0600

    New NavBar Design Done

---
+++ FrontEnd/src/app/arefin/page.js
@@ -0,0 +1,12 @@
+import Title from '@/components/shared/ReusableComponents/Title';
+import React from 'react';
+
+const arefinPage = () => {
+    return (
+        <div>
+            <Title title="Home"></Title>
+        </div>
+    );
+};
+
+export default arefinPage;
--- FrontEnd/src/app/layout.js
+++ FrontEnd/src/app/layout.js
@@ -1,9 +1,9 @@
 import Footer from '@/components/shared/Footer'
-import Navbar from '@/components/shared/Navbar'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
 import './globals.css'
+import Navbar from '@/components/shared/Navbar';
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'EchoScript',
@@ -13,7 +13,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      {/* <body className={inter.className}> */}
+      <body>
         <div className="max-w-screen-full mx-auto">
           <Navbar />
           <div className='container mx-auto'>{children}</div>
