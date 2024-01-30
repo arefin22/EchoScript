1f37a70822439027ee26d5652d681857e0954100
Stefany Cardozo <joydipbanik6@gmail.com>
2024-01-30 14:16:30 +0530

    [bugFixed](added editor and bug fixed in signup page)

---
+++ FrontEnd/src/app/dashboard/write/page.js
@@ -0,0 +1,12 @@
+import TextEditor from '@/components/TextEditor/TextEditor';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <TextEditor/>
+        </div>
+    );
+};
+
+export default page;
