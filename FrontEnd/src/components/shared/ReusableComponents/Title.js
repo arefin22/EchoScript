b6d841cb8b2fe2a5a4c648c6d8bf8a796c462888
Shamsul Arefin <shamsularefin500@gmail.com>
2024-01-30 14:07:48 +0600

    [Design](HomePage Responsive Done)

--- FrontEnd/src/components/shared/ReusableComponents/Title.js
+++ FrontEnd/src/components/shared/ReusableComponents/Title.js
@@ -1,7 +1,7 @@
 const Title = ({title}) => {
   return <div className="py-10">
-    <p className="text-9xl text-[#F1F1F1] -mb-12 -ml-2">{title}</p>
-    <h2 className="text-4xl text-[#4C2F17]">{title}</h2>
+    <p className="lg:text-9xl text-4xl text-[#F1F1F1] -mb-7  lg:-mb-12 lg:-ml-2">{title}</p>
+    <h2 className="lg:text-4xl text-xl text-[#4C2F17]">{title}</h2>
   </div>;
 };
 
