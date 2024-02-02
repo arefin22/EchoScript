e21ea9f319e167a641ddbe373bf9775313056af1
abdullahal5 <abdullahalfahin183@gmail.com>
2024-02-01 18:59:20 -0800

    backend user api

--- BackEnd/src/models/User.js
+++ BackEnd/src/models/User.js
@@ -1,23 +1,29 @@
 const { model, Schema } = require("mongoose");
 
 const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    photoURL:{
-        type: String,
-        required: true
-    },
-    role: {
-        type: String
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  photoURL: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: 'user'
+  },
+  favourite: {
+    type: [String],
+    default: [],
+    required: true
+  },
+});
 
 const User = model("user", UserSchema);
 
-module.exports = User
+module.exports = User;
