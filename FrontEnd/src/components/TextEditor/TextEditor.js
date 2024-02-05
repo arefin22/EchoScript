275c252c4f4277c345161be331f0fcac62f40855
Stefany Cardozo <joydipbanik6@gmail.com>
2024-02-05 14:33:01 +0530

    [successfully image upload](image upload into imageBB and show that image in the editor)

--- FrontEnd/src/components/TextEditor/TextEditor.js
+++ FrontEnd/src/components/TextEditor/TextEditor.js
@@ -8,14 +8,17 @@ import Quote from "@editorjs/quote";
 import ImageTool from "@editorjs/image";
 import CodeTool from "@editorjs/code";
 import "./TextEditor.css";
+import { axiosPublic } from "@/utils/useAxiosPublic";
 
 const TextEditor = () => {
   const ejInstance = useRef();
 
+  const image_hosting_api = `https://api.imgbb.com/1/upload?key=78e1a9dbe573d8923a63de7e43c7a68b`;
+
   const initEditor = () => {
     const editor = new EditorJS({
       holder: "editorjs",
-      minHeight : "200px",
+      minHeight: "200px",
       onReady: () => {
         ejInstance.current = editor;
       },
@@ -101,28 +104,37 @@ const TextEditor = () => {
           config: {
             uploader: {
               async uploadByFile(file) {
-                const formData = new FormData();
-                formData.append("file", file);
+                try {
+                  const formData = new FormData();
+                  formData.append("image", file);
 
-                const response = await axios.post(
-                  "https://api.imgbb.com/1/upload?key=a5d06824c299fd320f49135dcd5fa3dd",
-                  formData,
-                  {
-                    headers: {
-                      "Content-Type": "multipart/form-data",
-                    },
-                    withCredentials: false,
+                  const response = await axiosPublic.post(
+                    image_hosting_api,
+                    formData,
+                    {
+                      headers: {
+                        "Content-Type": "multipart/form-data",
+                      },
+                    }
+                  );
+
+                  if (response.data.success === true) {
+                    return {
+                      success: 1,
+                      file: {
+                        url: response.data.data.display_url,
+                      },
+                    };
+                  } else {
+                    return {
+                      success: 0,
+                      file: {
+                        url: null,
+                      },
+                    };
                   }
-                );
-
-                if (response.data.success === 1) {
-                  return {
-                    success: 1,
-                    file: {
-                      url: response.data.data.url,
-                    },
-                  };
-                } else {
+                } catch (error) {
+                  console.error("Error uploading image:", error);
                   return {
                     success: 0,
                     file: {
@@ -131,30 +143,30 @@ const TextEditor = () => {
                   };
                 }
               },
-              async uploadByUrl(url) {
-                const response = await axios.post(
-                  "https://api.imgbb.com/1/upload?key=a5d06824c299fd320f49135dcd5fa3dd",
-                  {
-                    url,
-                  }
-                );
+              // async uploadByUrl(url) {
+              //   const response = await axios.post(
+              //     "https://api.imgbb.com/1/upload?key=a5d06824c299fd320f49135dcd5fa3dd",
+              //     {
+              //       url,
+              //     }
+              //   );
 
-                if (response.data.success === 1) {
-                  return {
-                    success: 1,
-                    file: {
-                      url: response.data.data.url,
-                    },
-                  };
-                } else {
-                  return {
-                    success: 0,
-                    file: {
-                      url: null,
-                    },
-                  };
-                }
-              },
+              //   if (response.data.success === 1) {
+              //     return {
+              //       success: 1,
+              //       file: {
+              //         url: response.data.data.url,
+              //       },
+              //     };
+              //   } else {
+              //     return {
+              //       success: 0,
+              //       file: {
+              //         url: null,
+              //       },
+              //     };
+              //   }
+              // },
             },
             inlineToolbar: true,
           },
@@ -163,25 +175,25 @@ const TextEditor = () => {
     });
   };
 
-  // useEffect(() => {
-  //   if (ejInstance.current === null) {
-  //     initEditor();
-  //   }
+  useEffect(() => {
+    if (ejInstance.current === null) {
+      initEditor();
+    }
 
-  //   return () => {
-  //     ejInstance?.current?.destroy();
-  //     ejInstance.current = null;
-  //   };
-  // }, []);
+    return () => {
+      ejInstance?.current?.destroy();
+      ejInstance.current = null;
+    };
+  }, []);
 
-  initEditor();
+  // initEditor();
 
   const handleSave = async () => {
     const content = await ejInstance.current.saver.save();
     console.log(content);
 
     try {
-      const response = await axios.post("/api/saveArticle", { content });
+      // const response = await axios.post("/api/saveArticle", { content });
 
       if (response.data.success) {
         console.log("Article saved successfully.");
