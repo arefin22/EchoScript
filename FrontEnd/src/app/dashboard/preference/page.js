3a6ecda1711da81ba4bf80dd6d8fd334f1db7a4b
Stefany Cardozo <joydipbanik6@gmail.com>
2024-02-12 14:49:29 +0530

    [added category and tags](update editor with next button with conditional)

---
+++ FrontEnd/src/app/dashboard/preference/page.js
@@ -0,0 +1,11 @@
+import Preference from "@/components/Preference/Preference";
+
+const page = () => {
+    return (
+        <div>
+            <Preference/>
+        </div>
+    );
+};
+
+export default page;
