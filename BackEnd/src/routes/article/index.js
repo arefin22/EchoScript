137d2131da4f1fcc5e4c849b782f45953d09f7aa
abdullahal5 <abdullahalfahin183@gmail.com>
2024-02-13 15:04:02 -0800

    delete update method

--- BackEnd/src/routes/article/index.js
+++ BackEnd/src/routes/article/index.js
@@ -3,19 +3,14 @@ const likeOfComment = require("../../controllers/article/likeOfComment");
 const addAComment = require("../../controllers/article/putComment");
 const putLikes = require("../../controllers/article/putLikes");
 const getTextArticle = require("../../controllers/textArticle/getTextArticle");
-const getTextArticleByEmail = require("../../controllers/textArticle/getTextArticleByEmail");
 const getTextArticleById = require("../../controllers/textArticle/getTextArticleById");
 
 
 const router = require("express").Router();
 
-router.get("/article", getAllArticle)
-router.get("/textArticle", getTextArticle);
-router.get("/textArticle/:id", getTextArticleById);
-router.get("/textArticle", getTextArticleByEmail);
+
 router.put("/article/:id", addAComment);
 router.put("/article/:id/like", putLikes)
 // like of comment
 router.put("/article/:id", likeOfComment);
-
 module.exports = router
