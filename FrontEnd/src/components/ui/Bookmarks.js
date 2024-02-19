afbcf4038282a9e2c3dc5f1496b6e73ff55feb08
abdullahal5 <abdullahalfahin183@gmail.com>
2024-02-18 23:10:12 -0800

    bookmark1

--- FrontEnd/src/components/ui/Bookmarks.js
+++ FrontEnd/src/components/ui/Bookmarks.js
@@ -11,10 +11,14 @@ const Bookmarks = async ({user,data}) => {
           user: user,
           data: data,
         };
+<<<<<<< HEAD
         const { data: responseData } = await axiosSecure.post(
           "/bookmark",
           booked
         );
+=======
+        const {data: responseData} = await axiosSecure.post('/bookmark', booked);
+>>>>>>> 1f8b47da7d67d8bd0a1877a07a7a1dab37c12b0d
         toast.success("Thi item is Bookmarked successfully")
         return responseData;
       } catch (error) {
