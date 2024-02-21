7e0a3a9cb8181be3d6d197b144f56ae047d1953b
Stefany Cardozo <joydipbanik6@gmail.com>
2024-02-21 14:34:15 +0530

    change main layout, home page design. added trending component

--- FrontEnd/src/app/page.js
+++ FrontEnd/src/app/page.js
@@ -5,6 +5,7 @@ import ButtonWithBgColor from "@/components/Button/ButtonWithBgColor";
 import Card from "@/components/Card/Card";
 import Payment from "@/components/Payment/Payment";
 import Recomendation from "@/components/Recomendation/page";
+import Trending from "@/components/Trending/Trending";
 import Footer from "@/components/shared/Footer";
 import Navbar2 from "@/components/shared/Navbar2/Navbar2";
 import Title from "@/components/shared/ReusableComponents/Title";
@@ -15,49 +16,50 @@ import Link from "next/link";
 
 const page = () => {
     return (
-        <div>
-             
-      <Navbar2/>
-      <Banner/>
+      <div className="container mx-auto">
+        <Navbar2 />
+          <Banner />
+          <div className="mt-[-80px]">
+            <Trending />
+          </div>
 
-      <div className="container mx-auto my-24">
-        <Recomendation/>
-      </div>
-      {/* popular cards */}
-      <div className="container mx-auto my-24">
-        <Title title="Latest Article"></Title>
-        <div className="my-32 grid grid-cols-1 md:grid-cols-3 gap-10">
-          {cardData?.map((card, index) => (
-            <Link key={index} href="/singlearticle">
-              <Card
-                key={index}
-                tags={card.tags}
-                title={card.title}
-                date={card.date}
-                likes={card.likes}
-                comments={card.comments}
-                article={card.article}
-                image={card.image}
-                authorName={card.authorName}
-                view={card.view}
-              />
-            </Link>
-          ))}
-        </div>
+          {/* <Recomendation/> */}
+
+        {/* popular cards */}
         <div className="my-24">
-          <div className=" text-center mx-auto">
-            <ButtonWithBgColor name={"Explore More"} />
+          <Title title="Latest Article"></Title>
+          <div className="my-32 grid grid-cols-1 md:grid-cols-3 gap-10">
+            {cardData?.map((card, index) => (
+              <Link key={index} href="/singlearticle">
+                <Card
+                  key={index}
+                  tags={card.tags}
+                  title={card.title}
+                  date={card.date}
+                  likes={card.likes}
+                  comments={card.comments}
+                  article={card.article}
+                  image={card.image}
+                  authorName={card.authorName}
+                  view={card.view}
+                />
+              </Link>
+            ))}
+          </div>
+          <div className="my-24">
+            <div className=" text-center mx-auto">
+              <ButtonWithBgColor name={"Explore More"} />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="container mx-auto my-24">
-        <Title title="Packages"></Title>
-        <Payment />
-      </div>
-
-      <Footer/>
+        <div className="my-24">
+          <Title title="Packages"></Title>
+          <Payment />
         </div>
+
+        <Footer />
+      </div>
     );
 };
 
---
+++ FrontEnd/src/components/Trending/Trending.js
@@ -0,0 +1,105 @@
+import { FaCircle } from "react-icons/fa";
+
+const Trending = () => {
+  return (
+    <div className="bg-white rounded-tl-[100px] rounded-tr-[100px]">
+      <h2 className="px-20 py-5">
+        Trending On <span className="underline">EchoScript</span>
+      </h2>
+      <div className="grid grid-cols-2 gap-10 px-20 py-20 mx-20">
+        <div className="w-1/2 flex flex-col-2 gap-5">
+          <h2 className="text-gray-400">01</h2>
+          <div className="space-y-3">
+            <div className="flex items-center gap-3 mt-[-30px]">
+              <FaCircle />
+              <p>Author</p>
+            </div>
+            <h5 className="font-bold">
+              Lorem Ipsum is simply dummy text of the printing
+            </h5>
+            <small>5 Feb</small>
+          </div>
+        </div>
+
+        <div className="w-1/2 flex flex-col-2 gap-5">
+          <h2 className="text-gray-400">01</h2>
+          <div className="space-y-3">
+            <div className="flex items-center gap-3 mt-[-30px]">
+              <FaCircle />
+              <p>Author</p>
+            </div>
+            <h5 className="font-bold">
+              Lorem Ipsum is simply dummy text of the printing
+            </h5>
+            <small>5 Feb</small>
+          </div>
+        </div>
+      </div>
+      <div className="divider w-[90%] mx-auto"></div>
+
+      <div className="grid grid-cols-2 gap-10 px-20 py-20 mx-20">
+        <div className="w-1/2 flex flex-col-2 gap-5">
+          <h2 className="text-gray-400">01</h2>
+          <div className="space-y-3">
+            <div className="flex items-center gap-3 mt-[-30px]">
+              <FaCircle />
+              <p>Author</p>
+            </div>
+            <h5 className="font-bold">
+              Lorem Ipsum is simply dummy text of the printing
+            </h5>
+            <small>5 Feb</small>
+          </div>
+        </div>
+
+        <div className="w-1/2 flex flex-col-2 gap-5">
+          <h2 className="text-gray-400">01</h2>
+          <div className="space-y-3">
+            <div className="flex items-center gap-3 mt-[-30px]">
+              <FaCircle />
+              <p>Author</p>
+            </div>
+            <h5 className="font-bold">
+              Lorem Ipsum is simply dummy text of the printing
+            </h5>
+            <small>5 Feb</small>
+          </div>
+        </div>
+      </div>
+
+      <div className="divider w-[90%] mx-auto"></div>
+
+      <div className="grid grid-cols-2 gap-10 px-20 py-20 mx-20">
+        <div className="w-1/2 flex flex-col-2 gap-5">
+          <h2 className="text-gray-400">01</h2>
+          <div className="space-y-3">
+            <div className="flex items-center gap-3 mt-[-30px]">
+              <FaCircle />
+              <p>Author</p>
+            </div>
+            <h5 className="font-bold">
+              Lorem Ipsum is simply dummy text of the printing
+            </h5>
+            <small>5 Feb</small>
+          </div>
+        </div>
+
+        <div className="w-1/2 flex flex-col-2 gap-5">
+          <h2 className="text-gray-400">01</h2>
+          <div className="space-y-3">
+            <div className="flex items-center gap-3 mt-[-30px]">
+              <FaCircle />
+              <p>Author</p>
+            </div>
+            <h5 className="font-bold">
+              Lorem Ipsum is simply dummy text of the printing
+            </h5>
+            <small>5 Feb</small>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Trending;
