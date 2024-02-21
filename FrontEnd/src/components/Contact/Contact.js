1ff08eb804124d85f53cbf618c04a462b7995274
Stefany Cardozo <joydipbanik6@gmail.com>
2024-02-21 16:42:38 +0530

    new footer added

--- FrontEnd/src/components/Contact/Contact.js
+++ FrontEnd/src/components/Contact/Contact.js
@@ -1,9 +1,11 @@
 
 const Contact = () => {
     return (
-      <div className="bg-white text-center text-black py-20 space-y-20">
+      <div className="bg-white text-center text-black py-20 space-y-20 rounded-bl-[100px] rounded-br-[100px]">
         <p>Contact</p>
-        <h2 className="w-[50%] mx-auto text-5xl font-light">Would You Like To Have A Conversation With Us?</h2>
+        <h2 className="w-[50%] mx-auto text-5xl font-light">
+          Would You Like To Have A Conversation With Us?
+        </h2>
         <button className="bg-black rounded-full text-white px-10 py-3">
           Get in Touch
         </button>
