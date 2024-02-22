5978317de415669af1e5cee7af8d7f7381f6b2d3
joydip62 <joydipbanik6@gmail.com>
2024-02-22 22:08:58 +0530

    update bottom to top button

--- FrontEnd/src/components/Popular/Popular.js
+++ FrontEnd/src/components/Popular/Popular.js
@@ -34,7 +34,7 @@ const Popular = () => {
         </div>
         <div className="sticky top-0 bg-white rounded-tl-[100px] rounded-tr-[100px]">
           <div
-            className="w-full h-screen rounded-tl-[30px] rounded-tr-[30px] rounded-bl-[30px] rounded-br-[30px] lg:rounded-tl-[100px] lg:rounded-tr-[100px]"
+            className="w-full h-screen rounded-tl-[30px] rounded-tr-[30px] rounded-bl-[30px] rounded-br-[30px] lg:rounded-bl-[100px] lg:rounded-br-[100px] lg:rounded-tl-[100px] lg:rounded-tr-[100px]"
             style={{
               backgroundImage:
                 "url(https://i.ibb.co/6wxTt4W/1-RNy-Xe0v36rg8-X4i-Cp-QYFag.webp)",
