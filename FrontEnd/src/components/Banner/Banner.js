f9c403bcc03c4d026fe223cd17d02ea3acb17b64
Stefany Cardozo <joydipbanik6@gmail.com>
2024-02-26 12:59:47 +0530

    update responsive all pages

--- FrontEnd/src/components/Banner/Banner.js
+++ FrontEnd/src/components/Banner/Banner.js
@@ -13,27 +13,31 @@ const Banner = () => {
   return (
     <div className="mt-[-20px] lg:mt-[-50px] z-1">
       <div
-        className="w-full pb-96 rounded-tl-[30px] rounded-tr-[30px] lg:rounded-tl-[100px] lg:rounded-tr-[100px]"
+        className="rounded-tl-[30px] rounded-tr-[30px] lg:rounded-tl-[100px] lg:rounded-tr-[100px]"
         style={{
           // backgroundImage: "url(https://i.ibb.co/rsGgD7b/banner.jpg)",
           background:
             "linear-gradient(rgba(25,25,25,0),rgba(0,0,0,.9)), url(https://i.ibb.co/wQ4LPrQ/shunya-koide-1em-Wndl-DHs0-unsplash.jpg)",
           backgroundSize: "cover",
+          backgroundSize: "100%",
+          backgroundRepeat: "no-repeat",
+          backgroundAttachment: "fixed",
+          
         }}
       >
         <SubHeader onSearch={handleSearch} onClose={handleCloseSearchModal} />
-
-        <div className="w-full mt-5 lg:pt-10" data-aos="fade-up">
+        {/* data-aos="fade-up" */}
+        <div className="w-full mt-5 lg:pt-10">
           <div className="mx-auto text-center">
-            <h1 className="mb-5 text-white mx-auto text-center text-6xl font-[600px] w-[150px] lg:text-[200px] lg:w-[800px] ">
+            <h1 className="mb-5 text-white mx-auto text-center font-[600px] w-[200px] text-[50px] md:w-[300px] md:text-[100px] lg:text-[120px] lg:w-[500px]">
               Let Story's Shine
             </h1>
-            <p className="mb-5 text-3xl pt-20 text-white font-bold">
+            <p className="mb-5 text-xl md:text-3xl lg:text-5xl lg:pt-5 text-white font-bold">
               The Cozy Corner for Sharing Life Tales!
             </p>
           </div>
         </div>
-        <div className="text-white text-xl font-semibold mt-68 lg:mt-[200px] pl-10 lg:pb-0 lg:pl-24">
+        <div className="text-white text-xl font-semibold text-center pb-[80px] md:py-[120px] lg:py-[200px] lg:text-left lg:pl-24">
           <p>Loved Place - By Reader</p>
           <p>Read Now</p>
         </div>
--- FrontEnd/src/components/SubHeader/SubHeader.js
+++ FrontEnd/src/components/SubHeader/SubHeader.js
@@ -10,8 +10,8 @@ import Link from "next/link";
 
 const SubHeader = ({ onSearch, onClose }) => {
   return (
-    <div className="w-full flex justify-between py-5 px-3 lg:px-20 lg:pt-10 items-center">
-      <div className="w-40 lg:w-full">
+    <div className="w-full flex justify-between py-5 px-5 lg:px-10 xl:px-20 lg:pt-10 items-center">
+      <div className="w-[150px] md:w-[200px] xl:w-full">
         {/* navbar left part */}
         <Link href={"/"}>
           <Image
@@ -21,7 +21,7 @@ const SubHeader = ({ onSearch, onClose }) => {
           />
         </Link>
       </div>
-      <div className="bg-black rounded-full lg:py-[11px] lg:px-[5px]">
+      <div className="bg-black rounded-full xl:py-[11px] xl:px-[5px]">
         {/* navbar right part */}
         <Search onSearch={onSearch} onClose={onClose} />
       </div>
