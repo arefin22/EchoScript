c16c021854153b2b652bcb8814c5799591d08ded
Shamsul Arefin <shamsularefin500@gmail.com>
2024-02-27 02:08:29 +0600

    Payment Successfull

--- BackEnd/src/controllers/payment/addAPayment.js
+++ BackEnd/src/controllers/payment/addAPayment.js
@@ -1,12 +1,13 @@
 const stripe = require("stripe")(process.env.STRIPE_TOKEN);
 
 const addAPayment = async (req, res) => {
-  const { amount } = req.body;
+  const { amount , customer } = req.body;
   const price = amount * 100;
   const paymentIntent = await stripe.paymentIntents.create({
     amount: price,
     currency: "usd",
     payment_method_types: ["card"],
+    receipt_email: customer,
   });
 
   res.send({
