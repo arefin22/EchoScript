54245531be1f0011363f1c75f122dfab9bf4545d
Stefany Cardozo <joydipbanik6@gmail.com>
2024-02-29 10:48:35 +0530

    update home navbar

--- FrontEnd/src/app/packages/page.js
+++ FrontEnd/src/app/packages/page.js
@@ -16,8 +16,8 @@ const page = () => {
     setCategoryFilter("All");
   };
   return (
-    <div className="mx-auto px-4 lg:px-6 lg:pt-5">
-      <div className="mx-auto sticky z-50 -mt-7 top-[40px] md:-mt-8 md:top-[40px] lg:-mt-14 lg:w-[45%] lg:top-[65px] xl:w-[35%] xl:top-[60px] xl:-mt-18 2xl:w-[25%]">
+    <div className="z-1 px-6 pt-5 mt-[-20px] lg:mt-[-40px]">
+      <div className="mx-auto sticky z-50 -mt-3 top-[40px] md:-mt-4 md:top-[40px] lg:-mt-2 lg:w-[45%] lg:top-[65px] xl:w-[35%] xl:top-[60px] xl:-mt-2">
         <Navbar />
       </div>
 
