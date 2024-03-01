ec976b6c3ab02799b9902989007b6b8e683d637c
Stefany Cardozo <joydipbanik6@gmail.com>
2024-03-01 13:30:26 +0530

    added papular post on home page with dynamic data

--- FrontEnd/src/components/Popular/Popular.js
+++ FrontEnd/src/components/Popular/Popular.js
@@ -1,11 +1,54 @@
-import Image from "next/image";
+"use client";
 
-
+import { axiosSecure } from "@/utils/useAxiosSecure";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const Popular = () => {
-    return (
-      <div className="bg-white z-1">
-        <div className="sticky top-0 lg:mt-[-100px]">
+  const [thumbnails, setThumbnails] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axiosSecure.get("/textArticle").then((res) => {
+      
+      const sortedData = res.data.sort(
+        (a, b) => b.likes.length - a.likes.length
+      );
+// console.log(sortedData);
+      const thumbnails = sortedData.slice(0, 4).map((item) => ({
+        thumbnail: item.texteditor?.thumbnail,
+        postId: item._id,
+
+      }));
+
+
+      setThumbnails(thumbnails);
+      setLoading(false);
+    });
+  }, [axiosSecure]);
+
+  // console.log(data.map((d) => console.log(d.texteditor?.thumbnail)));
+  // console.log(thumbnails);
+
+  return (
+    <div className="bg-white z-1">
+      {thumbnails.map((thumbnail, index) => (
+        <div key={index} className="sticky top-0 lg:mt-[-100px]">
+          <Link href={`/article/${thumbnail.postId}`}>
+            <div
+              className="w-full h-screen rounded-tl-[30px] rounded-tr-[30px] rounded-bl-[30px] rounded-br-[30px] lg:rounded-tl-[100px] lg:rounded-tr-[100px] lg:rounded-bl-[100px] lg:rounded-br-[100px]"
+              style={{
+                backgroundImage: `url(${thumbnail.thumbnail})`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+              }}
+            ></div>
+          </Link>
+        </div>
+      ))}
+
+      {/* <div className="sticky top-0 lg:mt-[-100px]">
           <div
             className="w-full h-screen rounded-tl-[30px] rounded-tr-[30px] lg:rounded-tl-[100px] lg:rounded-tr-[100px]"
             style={{
@@ -15,46 +58,9 @@ const Popular = () => {
               backgroundPosition: "center",
             }}
           ></div>
-        </div>
-
-        <div className="sticky top-0 lg:mt-[-100px]">
-          <div
-            className="w-full h-screen rounded-tl-[30px] rounded-tr-[30px] lg:rounded-tl-[100px] lg:rounded-tr-[100px]"
-            style={{
-              backgroundImage:
-                "url(https://i.ibb.co/Vwcwgwn/1-z-5sz44-FOBGUwh-Qqpl-Fotw.webp)",
-              backgroundSize: "cover",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-            }}
-          ></div>
-        </div>
-        <div className="sticky top-0 lg:mt-[-100px]">
-          <div
-            className="w-full h-screen rounded-tl-[30px] rounded-tr-[30px] lg:rounded-tl-[100px] lg:rounded-tr-[100px]"
-            style={{
-              backgroundImage:
-                "url(https://i.ibb.co/n3b56qG/0-dxp-NPWAOOa-F-Lcx-B.webp)",
-              backgroundSize: "cover",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-            }}
-          ></div>
-        </div>
-        <div className="sticky top-0 bg-white rounded-tl-[100px] rounded-tr-[100px]">
-          <div
-            className="w-full h-screen rounded-tl-[30px] rounded-tr-[30px] rounded-bl-[30px] rounded-br-[30px] lg:rounded-bl-[100px] lg:rounded-br-[100px] lg:rounded-tl-[100px] lg:rounded-tr-[100px]"
-            style={{
-              backgroundImage:
-                "url(https://i.ibb.co/6wxTt4W/1-RNy-Xe0v36rg8-X4i-Cp-QYFag.webp)",
-              backgroundSize: "cover",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-            }}
-          ></div>
-        </div>
-      </div>
-    );
+        </div> */}
+    </div>
+  );
 };
 
 export default Popular;
